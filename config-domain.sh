9868ab07c54507192cea8edee8b34e704c043fae
luke crouch <luke.crouch@gmail.com>
2021-08-27 10:44:19 -0500

    new GitHub Action workflows for lint and continuous prereleases

---
+++ config-domain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# script to run the sed commands in both OSX and linux (e.g., GitHub Action)
+# the commands ...
+# first remove the :8000 part of the http://127.0.0.1:8000 domain
+# then replace all http://127.0.0.1 with the domain passed to the script
+
+echo $1
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/:8000//g"  src/manifest.json src/js/background.js src/popup.html
+    sed -i '' "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background.js src/popup.html
+else
+    sed -i "s/:8000//g"  src/manifest.json src/js/background.js src/popup.html
+    sed -i "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background.js src/popup.html
+fi
