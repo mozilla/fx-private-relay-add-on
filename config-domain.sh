eff33e3675bddb71bd173e6bb5ee372b6cadd6dd
Vincent <git@vincenttunru.com>
2021-11-19 17:54:51 +0100

    Fix path to JS file

--- config-domain.sh
+++ config-domain.sh
@@ -8,9 +8,9 @@
 echo $1
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/:8000//g"  src/manifest.json src/js/background.js src/popup.html
-    sed -i '' "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background.js src/popup.html
+    sed -i '' "s/:8000//g"  src/manifest.json src/js/background/background.js src/popup.html
+    sed -i '' "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background/background.js src/popup.html
 else
-    sed -i "s/:8000//g"  src/manifest.json src/js/background.js src/popup.html
-    sed -i "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background.js src/popup.html
+    sed -i "s/:8000//g"  src/manifest.json src/js/background/background.js src/popup.html
+    sed -i "s|http://127.0.0.1|$1|g" src/manifest.json src/js/background/background.js src/popup.html
 fi
