e1113d28113dd11c3a048fd492e9602a0568cafb
Maxx Crawford <maxx.crawford@gmail.com>
2022-03-22 14:55:08 -0500

    Send browser vendor with telemetry events

--- src/js/shared/metrics.js
+++ src/js/shared/metrics.js
@@ -4,12 +4,14 @@
 
 // eslint-disable-next-line no-redeclare
 async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
+  const browserVendor = browser.menus ? "Firefox" : "Chrome";
   return await browser.runtime.sendMessage({
     method: "sendMetricsEvent",
     eventData: {
       category: `Extension: ${eventCategory}`,
       action: eventAction,
       label: eventLabel,
+      dimension5: browserVendor
     },
   });
 }
