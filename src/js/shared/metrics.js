288fa3c39ed20f120a94f4d38785843c682a5349
Maxx Crawford <maxx.crawford@gmail.com>
2022-03-23 09:12:04 -0500

    Send browser vendor with telemetry events (#301)
    * Send browser vendor with telemetry events
    
    * Add comment for variable name

--- src/js/shared/metrics.js
+++ src/js/shared/metrics.js
@@ -4,12 +4,16 @@
 
 // eslint-disable-next-line no-redeclare
 async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
+  // "dimension5" is a Google Analytics-specific variable to track a custom dimension. 
+  // This dimension is used to determine which browser vendor the add-on is using: Firefox or Chrome
+  const browserVendor = browser.menus ? "Firefox" : "Chrome";
   return await browser.runtime.sendMessage({
     method: "sendMetricsEvent",
     eventData: {
       category: `Extension: ${eventCategory}`,
       action: eventAction,
       label: eventLabel,
+      dimension5: browserVendor
     },
   });
 }
