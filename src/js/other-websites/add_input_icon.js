9b31315bb3db9bc93e558b33aad5f7b96b3d9072
Maxx Crawford <maxx.crawford@gmail.com>
2022-05-09 11:38:30 -0500

    Add-on runs on Android (#306)
    * Update web-ext to support Fenix add-on development, add command to run add-on on Android
    
    * Add starting documentation
    
    * Add meta viewport tag for mobile layouts
    
    * Add logic to account for menu on small screens
    
    * Make unit variables ALL CAPS
    
    * Bump minimum version number to account for browserAction.X features for Firefox for Android
    
    * Update icons (lint error), switch context-menus back to 'all'
    
    * Add comment

--- src/js/other-websites/add_input_icon.js
+++ src/js/other-websites/add_input_icon.js
@@ -1,3 +1,5 @@
+const RELAY_INPAGE_MENU_WIDTH = 320;
+
 function closeRelayInPageMenu() {
   const relayIconBtn = document.querySelector(".fx-relay-menu-open");
   relayIconBtn?.classList.remove("fx-relay-menu-open");
@@ -33,21 +35,34 @@ function positionRelayMenu() {
   const relayInPageMenuIframe = document.querySelector(".fx-relay-menu-iframe");
   const newIconPosition = relayIconBtn.getBoundingClientRect();
   const documentPosition = document.documentElement.getBoundingClientRect();
-  const relayInPageMenuMaximumHeight = 405
+  const RELAY_INPAGE_MENU_MAXIMUM_HEIGHT = 405
   
   // Calculate the "safe area" of add-on in-page menu. If there's not enough room to expand below the icon, it expands above. 
-  // The relayInPageMenuMaximumHeight / 405 is the pixel height of the tallest version of the inpage menu. 
-  const positionMenuBelowIcon = ((((newIconPosition.top - documentPosition.top) - document.documentElement.scrollHeight) * -1) > relayInPageMenuMaximumHeight);
+  // The RELAY_INPAGE_MENU_MAXIMUM_HEIGHT / 405 is the pixel height of the tallest version of the inpage menu. 
+  const positionMenuBelowIcon = ((((newIconPosition.top - documentPosition.top) - document.documentElement.scrollHeight) * -1) > RELAY_INPAGE_MENU_MAXIMUM_HEIGHT);
+
+  function _setPositionX (newIconPosition) {
+    // This sets the inpage menu slightly offset from the Relay icon in the email input
+    // This function checks to make sure there's enough space for that offset
+    const offsetX = (window.innerWidth < 500) ? (RELAY_INPAGE_MENU_WIDTH - 32) : 255
+    return newIconPosition.x - offsetX + "px";
+  }
 
   if (positionMenuBelowIcon) {
-    relayInPageMenu.style.left = newIconPosition.x - 255 + "px";
+    relayInPageMenu.style.left = _setPositionX(newIconPosition);
     relayInPageMenu.style.top = newIconPosition.top + 40 + "px";
     relayInPageMenuIframe.classList.remove("is-position-bottom");
   } else {
-    relayInPageMenu.style.left = newIconPosition.x - 255 + "px";
+    relayInPageMenu.style.left = _setPositionX(newIconPosition);
     const relayInPageMenuIframeElement = document.querySelector(".fx-relay-menu-iframe iframe");
     relayInPageMenuIframe.classList.add("is-position-bottom")
     relayInPageMenu.style.top = newIconPosition.top - relayInPageMenuIframeElement.clientHeight - 20 + "px";
+  }
+
+  // Set arrow pointing to logo based on screenwidth
+  relayInPageMenuIframe.classList.remove("is-position-right");
+  if (window.innerWidth < 500) {
+    relayInPageMenuIframe.classList.add("is-position-right");
   }
 }
 
@@ -71,7 +86,7 @@ function buildInpageIframe(opts) {
   );
   const iframe = document.createElement("iframe");
   iframe.src = browser.runtime.getURL("inpage-panel.html");
-  iframe.width = 320;
+  iframe.width = RELAY_INPAGE_MENU_WIDTH;
   // This height is derived from the Figma file. However, this is just the starting instance of the iframe/inpage menu. After it's built out, it resizes itself based on the inner contents.
   iframe.height = 300;
   iframe.title = browser.i18n.getMessage("pageInputTitle");
