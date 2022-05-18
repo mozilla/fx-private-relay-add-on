a00a5160f0a2e49b663024db2811283d129d9a6d
Vincent <git@vincenttunru.com>
2022-05-18 15:10:09 +0200

    Respond to website events
    The website added some more events that the add-on can respond to.
    Specifically, we now ensure that our local cache of data is updated
    whenever the user registers their subdomain on the website, or adds
    or removes a mask on the website. We also now refresh non-settings
    pages whenever server-side storage of labels is toggled based on a
    website event.

--- src/js/relay.firefox.com/settings_refresh.js
+++ src/js/relay.firefox.com/settings_refresh.js
@@ -1,3 +1,16 @@
 (async function () {
-  browser.storage.local.set({ settingsRefresh: false });
+  document.querySelector(
+    "firefox-private-relay-addon"
+  )?.addEventListener("website", async (event) => {
+    if (event.detail.type === "serverStorageChange") {
+      // Reload all website pages, so that they can adjust to the user's new
+      // settings, i.e. to ensure that label changes are no longer sent to the
+      // server if server label storage is disabled, or vice versa,
+      // and that the add-on can re-initialise w.r.t. the data it injects and
+      // listens for in the website:
+      browser.runtime.sendMessage({
+        method: "refreshAccountPages",
+      });
+    }
+  });
 })();
