c1da4fed0b939ba61fd41545ff557126e180ed1b
Maxx Crawford <maxx.crawford@gmail.com>
2022-05-25 14:24:04 -0500

    Add logic to store used_on data for local labels

--- src/js/other-websites/inpage_menu.js
+++ src/js/other-websites/inpage_menu.js
@@ -132,18 +132,27 @@ async function fillTargetWithRelayAddress(generateClickEvt) {
   // If the used_on field is blank, then just set it to the current page/hostname. Otherwise, add/check if domain exists in the field
   const used_on = (currentUsedOnValue === null || currentUsedOnValue === undefined || currentUsedOnValue === "") ? `${currentPageHostName},` : addUsedOnDomain(currentUsedOnValue, currentPageHostName)
   
-  // Update server info with site usage
-  await browser.runtime.sendMessage({
-    method: "patchMaskInfo",
-    id: parseInt(maskAddressId, 10),
-    data: {
-      used_on
-    },
-    options: {
-      auth: true,
-      mask_type: maskObject.mask_type,
-    },
-  });
+  const serverStoragePref = await getCachedServerStoragePref();
+  
+  if (serverStoragePref) {
+    // Update server info with site usage
+    await browser.runtime.sendMessage({
+      method: "patchMaskInfo",
+      id: parseInt(maskAddressId, 10),
+      data: {
+        used_on
+      },
+      options: {
+        auth: true,
+        mask_type: maskObject.mask_type,
+      },
+    });
+  } else {
+    // Add in used_on to current mask object and re-sync labels
+    // TODO/BUG: Deleting masks drops this data
+    maskObject.used_on = used_on;
+    browser.storage.local.set({ relayAddresses: masks });
+  }
 
   // TODO: Add telemetry event (?)
 
