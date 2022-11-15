335de374e3eba975d4dadd53a3b47dd91d9fab53
Maxx Crawford <maxx.crawford@gmail.com>
2022-11-15 15:34:26 -0600

    Add additional custom dimension to track traffic from the add-on

--- src/js/shared/metrics.js
+++ src/js/shared/metrics.js
@@ -6,6 +6,8 @@
 async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
   // "dimension5" is a Google Analytics-specific variable to track a custom dimension. 
   // This dimension is used to determine which browser vendor the add-on is using: Firefox or Chrome
+  // "dimension7" is a Google Analytics-specific variable to track a custom dimension,
+  // used to determine which where the traffic is coming from: website, add-on or app
   return await browser.runtime.sendMessage({
     method: "sendMetricsEvent",
     eventData: {
@@ -13,6 +15,7 @@ async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
       action: eventAction,
       label: eventLabel,
       dimension5: await getBrowser(),
+      dimension7: "add-on",
     },
   });
 }
