8db963b924150787feb0e7345c59a26e0178389f
Kaitlyn <kandres@mozilla.com>
2022-12-29 11:40:58 -0500

    MPP-2534 - Do not display negative masks in popup

--- src/js/popup/popup.js
+++ src/js/popup/popup.js
@@ -464,10 +464,17 @@ async function showRelayPanel(tipPanelToShow) {
   const { relayAddresses, maxNumAliases } = await getRemainingAliases();
   const numRemaining = maxNumAliases - relayAddresses.length;
   const remainingAliasMessage = document.querySelector(".aliases-remaining");
-  remainingAliasMessage.textContent = browser.i18n.getMessage("popupRemainingAliases_2_mask", [numRemaining, maxNumAliases]);
   const getUnlimitedAliases = document.querySelector(".premium-cta");
   getUnlimitedAliases.textContent = browser.i18n.getMessage("popupGetUnlimitedAliases_mask");
   document.body.classList.add("relay-panel");
+  
+  // Prevent negative masks from showing, default to 0 if all free masks have been used up
+  let positiveNumRemaining = numRemaining;
+  if (numRemaining <= 0) {
+    positiveNumRemaining = 0;
+  }
+  remainingAliasMessage.textContent = browser.i18n.getMessage("popupRemainingAliases_2_mask", [positiveNumRemaining, maxNumAliases]);
+  
   updatePremiumPanel(tipPanelToShow);
   updatePanel(numRemaining, tipPanelToShow);
 
