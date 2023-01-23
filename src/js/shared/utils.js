1165c49e1a5e137498aebf38e32fe3238d64a9e6
Maxx Crawford <maxx.crawford@gmail.com>
2023-01-23 16:39:25 -0600

    Move getBrowser function to shared utils

--- src/js/shared/utils.js
+++ src/js/shared/utils.js
@@ -1,4 +1,4 @@
-/* exported areInputIconsEnabled setCustomFonts preventDefaultBehavior */
+/* exported areInputIconsEnabled setCustomFonts preventDefaultBehavior checkWaffleFlag getBrowser */
 
 // eslint-disable-next-line no-redeclare
 async function areInputIconsEnabled() {
@@ -52,4 +52,17 @@ async function checkWaffleFlag(flag) {
     }
   }
   return false;
+}
+
+// eslint-disable-next-line no-unused-vars
+async function getBrowser() {
+  if (typeof browser.runtime.getBrowserInfo === "function") {
+    /** @type {{ name: string, vendor: string, version: string, buildID: string }} */
+    const browserInfo = await browser.runtime.getBrowserInfo();
+    return browserInfo.name;
+  }
+  if (navigator.userAgent.toLowerCase().indexOf("firefox") !== -1) {
+    return "Firefox";
+  }
+  return "Chrome";
 }
