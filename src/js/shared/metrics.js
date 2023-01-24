0c1c9c3d20900bb5c3651ec09cae4a688df331a4
Maxx Crawford <maxx.crawford@gmail.com>
2023-01-24 10:09:32 -0600

    [MPP-1714] Popup Refactor - Report Issue Panel (#457)
    * Stub new work area for toolbar
    
    * Add new navigation icons
    
    * Add hard-coded (EN) labels for navigation
    
    * Start popup javascript logic (including init, panel switching, and login detection)
    
    * Remove commented out code
    
    * FOLD SVG name update
    
    * Add logic to handle external link clicks in popup, update HTML with data attrs to set URLs correctly with telemetry data
    
    * Add back button support for main panels
    
    * Create settings page content, style toggle buttons to match Nebula
    
    * Stub Issue Report page, add remaining Settings page external link
    
    * Add logic to handle child panel navigation
    
    * Fix issue where external links where not opening to correct URL
    
    * Move getBrowser function to shared utils
    
    * Optimize Report Issue success image
    
    * Add Report Issue panel content/logic from previous popup file

--- src/js/shared/metrics.js
+++ src/js/shared/metrics.js
@@ -1,10 +1,9 @@
-"use strict";
-
+/* global getBrowser */
 /* exported sendRelayEvent */
 
 // eslint-disable-next-line no-redeclare
 async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
-  // "dimension5" is a Google Analytics-specific variable to track a custom dimension. 
+  // "dimension5" is a Google Analytics-specific variable to track a custom dimension.
   // This dimension is used to determine which browser vendor the add-on is using: Firefox or Chrome
   // "dimension7" is a Google Analytics-specific variable to track a custom dimension,
   // used to determine where the ping is coming from: website, add-on or app
@@ -18,16 +17,4 @@ async function sendRelayEvent(eventCategory, eventAction, eventLabel) {
       dimension7: "add-on",
     },
   });
-}
-
-async function getBrowser() {
-  if (typeof browser.runtime.getBrowserInfo === "function") {
-    /** @type {{ name: string, vendor: string, version: string, buildID: string }} */
-    const browserInfo = await browser.runtime.getBrowserInfo();
-    return browserInfo.name;
-  }
-  if (navigator.userAgent.toLowerCase().indexOf("firefox") !== -1) {
-    return "Firefox";
-  }
-  return "Chrome";
 }
