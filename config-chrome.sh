c905effe827f23901c78175a92515d41e7965de3
maxxcrawford <maxx.crawford@gmail.com>
2023-11-20 11:41:07 -0600

    Fix #MPP-3581 - Ship both context menu permissions for Firefox

--- config-chrome.sh
+++ config-chrome.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # script to run the sed commands in both OSX and linux (e.g., GitHub Action)
-# replace "menus" permission (Firefox) with Chrome-compability permission ("contextMenus")
+# remove "contextMenus" permission (Firefox-only) from manifest.json
+# Note: the last permission cannot be "menus" or the JSON file will create a linter error
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s|menus|contextMenus|g" src/manifest.json
+    sed -i '' "/menus/d" src/manifest.json
 else
-    sed -i "s|menus|contextMenus|g" src/manifest.json
+    sed -i "/menus/d" src/manifest.json
 fi
