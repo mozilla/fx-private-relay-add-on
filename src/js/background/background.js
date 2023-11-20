594f952f180adb4a1e16e1f4f91e16d1b5386019
Maxx Crawford <maxx.crawford@gmail.com>
2023-11-20 08:59:21 -0600

    Update src/js/background/background.js
    Co-authored-by: Peter deHaan <peter@deseloper.com>

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -1,5 +1,5 @@
 function startupInit() {
-  const RELAY_SITE_ORIGIN = "http://127.0.0.1:8000";  
+  const RELAY_SITE_ORIGIN = "http://127.0.0.1:8000";
   browser.storage.local.set({ RELAY_SITE_ORIGIN });
   browser.storage.local.set({ maxNumAliases: 5 });
   browser.storage.local.set({ relaySiteOrigin: RELAY_SITE_ORIGIN });
